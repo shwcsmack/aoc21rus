c4ad16a130bb9147211e3aaa373d634a840d2311
Shayne Stewart <sh.wc.smack@gmail.com>
2021-12-09 14:10:28 -0600

    day3

---
+++ src/day3.rs
@@ -0,0 +1,60 @@
+#![feature(drain_filter)]
+
+const FILE: &str = "data/day3data.txt";
+const WIDTH: usize = 12;
+const COUNT: usize = 1000;
+
+pub fn part_one() -> u32 {
+    let gamma = crate::util::read_file(FILE)
+        .lines()
+        .map(|l| usize::from_str_radix(l, 2).unwrap())
+        .fold(vec![0; WIDTH], |count, bits| {
+            count
+                .into_iter()
+                .enumerate()
+                .map(|(i, n)| n + ((bits & 1 << i) >> i))
+                .collect()
+        })
+        .into_iter()
+        .enumerate()
+        .map(|(i, b)| ((b >= COUNT / 2) as u32) << i)
+        .sum::<u32>();
+
+    gamma * (!gamma & ((1 << WIDTH) - 1))
+}
+
+pub fn part_two() -> u32 {
+    let nums = crate::util::read_file(FILE)
+        .lines()
+        .map(|l| u32::from_str_radix(l, 2).unwrap())
+        .collect::<Vec<_>>();
+
+    let oxy = (0..WIDTH)
+        .rev()
+        .scan(nums.clone(), |oxy, i| {
+            let one = oxy.iter().filter(|n| *n & 1 << i > 0).count() >= (oxy.len() + 1) / 2;
+            oxy.drain_filter(|n| (*n & 1 << i > 0) != one);
+            oxy.first().copied()
+        })
+        .last()
+        .unwrap();
+
+    let co2 = (0..WIDTH)
+        .rev()
+        .scan(nums, |co2, i| {
+            let one = co2.iter().filter(|n| *n & 1 << i > 0).count() >= (co2.len() + 1) / 2;
+            co2.drain_filter(|n| (*n & 1 << i > 0) == one);
+            co2.first().copied()
+        })
+        .last()
+        .unwrap();
+
+    oxy * co2
+}
+// fn get_data() -> Vec<u16> {
+//     let file = crate::util::read_file(FILE);
+//     for line in file.lines() {
+//
+//     }
+//     vec![0]
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+#![feature(drain_filter)]
+
 mod day1;
 mod day2;
 mod util;
+mod day3;
 
 
 fn main() {
@@ -8,4 +11,6 @@ fn main() {
     println!("{:?}", day1::part_two());
     println!("{:?}", day2::part_one());
     println!("{:?}", day2::part_two());
+    println!("{:?}", day3::part_one());
+    println!("{:?}", day3::part_two());
 }
